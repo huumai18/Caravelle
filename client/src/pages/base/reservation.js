43d4c7f97136adbf94fe5deb0493743d68b8c352
huumai1998 <huumai0918@gmail.com>
2022-04-30 18:33:23 -0600

    UpdateReservationMainPage

--- client/src/pages/base/reservation.js
+++ client/src/pages/base/reservation.js
@@ -1,4 +1,10 @@
-import { Narrow, reservation1, reservation2 } from "../../images";
+import {
+  Narrow,
+  reservation1,
+  reservation2,
+  reservation3,
+  reservation4,
+} from "../../images";
 import { Button } from "@mui/material";
 import React from "react";
 
@@ -28,6 +34,8 @@ export const Reservation = () => {
         <div className="hero">
           <img className="model-left" src={reservation1} alt="model" />
           <img className="model-right" src={reservation2} alt="model" />
+          <img className="model-right2" src={reservation3} alt="model" />
+          <img className="model-left2" src={reservation4} alt="model" />
         </div>
       </div>
     </div>
