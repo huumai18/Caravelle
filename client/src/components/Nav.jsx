9accde7e73952d2c9cc9e4e88a62252053dfc08b
Van Hien <hiench2001@gmail.com>
2022-05-03 15:02:14 -0600

    added breakpoints and completed nav bar for mobile

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,55 +1,94 @@
 import { Link } from "react-scroll";
-import React from "react";
+import React, { useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
+
 export const Nav = () => {
+  const [open, setOpen] = useState(false);
+  const [close, setClose] = useState(false);
+
   return (
     <section className="nav">
       <div className="container">
-        <Link
-          to="home"
-          activeClass="active"
-          className="link"
-          smooth={true}
-          offset={-10}
-          spy={true}
-          duration={700}
-        >
-          Home
-        </Link>
-        <Link
-          activeClassName="active"
-          d
-          className="link"
-          to="about"
-          spy={true}
-          smooth={true}
-          offset={10}
-          duration={700}
-        >
-          About
-        </Link>
-        <Link
-          activeClassName="active"
-          className="link"
-          to="reservation"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={800}
-        >
-          Reservation
-        </Link>
+        <h2>Caravelle</h2>
+        <Hamburger
+          size={25}
+          color="white"
+          rounded
+          toggled={close}
+          toggle={setClose}
+          aria-expanded={close}
+          onToggle={(toggled) => {
+            if (toggled) {
+              setOpen(true);
+            } else {
+              setOpen(false);
+            }
+          }}
+        />
+        <div className={`nav-links ${open ? "" : "closed"}`}>
+          <Link
+            to="home"
+            activeClass="active"
+            className="link"
+            smooth={true}
+            offset={-10}
+            spy={true}
+            duration={700}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            activeClassName="active"
+            d
+            className="link"
+            to="about"
+            spy={true}
+            smooth={true}
+            offset={10}
+            duration={700}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            About
+          </Link>
+          <Link
+            activeClassName="active"
+            className="link"
+            to="reservation"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={800}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            Reservation
+          </Link>
 
-        <Link
-          className="link"
-          activeClassName="active"
-          to="location"
-          spy={true}
-          smooth={true}
-          offset={0}
-          duration={1200}
-        >
-          Location
-        </Link>
+          <Link
+            className="link"
+            activeClassName="active"
+            to="location"
+            spy={true}
+            smooth={true}
+            offset={0}
+            duration={1200}
+            onClick={() => {
+              setClose(false);
+              setOpen(false);
+            }}
+          >
+            Location
+          </Link>
+        </div>
       </div>
     </section>
   );
